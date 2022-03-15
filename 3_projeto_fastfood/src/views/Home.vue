d8a7a7501601a84ed2048124b565d25231d5bfe3
krokreti <davipinheiro1993@gmail.com>
2022-03-14 22:22:58 -0300

    Commit Inserçao de Dados

---
+++ 3_projeto_fastfood/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <Banner/>
+    <div class="main-container">
+        <h1>Monte o seu hamburguer: </h1>
+        <BurgerForm/>
+    </div>
+</div>
+</template>
+
+<script>
+import Banner from '../components/Banner.vue'
+import BurgerForm from '../components/BurgerForm.vue'
+
+
+export default {
+    components: {
+        Banner,
+        BurgerForm
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
