b775886595c6ac426679d831e39507854b3b5bd7
krokreti <davipinheiro1993@gmail.com>
2022-03-14 22:22:17 -0300

    Commit para Inserçao de Dados

--- 3_projeto_fastfood/src/App.vue
+++ 3_projeto_fastfood/src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+<div>
+  <Navbar :logo="logo_src" :alt="app_name" />
   <router-view/>
+  <Footer/>
+</div>
 </template>
 
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Hamburguer"
+    }
+  },  
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+  * {
+    font-family: Helvetica;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  .main-container {
+    min-height: 400px;
+    margin: 50px;
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222
+  }
+
 </style>
