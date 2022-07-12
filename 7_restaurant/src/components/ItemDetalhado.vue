b5430b5ccd09ea0bbc29d31cf88026f64423211b
davi pinheiro <davipinheiro1993@gmail.com>
2022-07-12 14:52:35 -0300

    Update button favorite and return
    Terminado o design do botão de retornar e o botão de favoritar

--- 7_restaurant/src/components/ItemDetalhado.vue
+++ 7_restaurant/src/components/ItemDetalhado.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="item-detalhado">
         <div class="btn-voltar" @click="voltar"><i class="fa fa-angle-left" aria-hidden="true"></i></div>
-        <div class="btn-favorito">Favorito</div>
+        <div class="btn-favorito" @click="favoritar">
+            <i class="fa-solid fa-heart" style="color: red;" v-if="favorite"></i>
+            <i class="fa-regular fa-heart" v-else></i>
+        </div>
         <div class="item-detalhado-imagem"></div>
         <div class="item-detalhado-sobre"></div>
     </div>
@@ -17,7 +20,7 @@ export default {
 
     data() {
         return {
-            
+            favorite: false,
         }
     },
     created() {
@@ -27,6 +30,9 @@ export default {
         voltar() {
             router.push({ name: 'cardapio' })
             console.log('voltei')
+        },
+        favoritar() {
+            this.favorite = !this.favorite
         }
     },
 }
