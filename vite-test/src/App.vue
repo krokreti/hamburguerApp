caa7259c01310755b18a1e43f09a93b6a14516ee
davi pinheiro <davipinheiro1993@gmail.com>
2022-07-12 15:34:12 -0300

    Create Vite Test
    Testando vite

---
+++ vite-test/src/App.vue
@@ -0,0 +1,55 @@
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" style="width:70px"/>
+  <h1>Seja Bem-Vindo(a)</h1>
+  <hr>
+    <div class="items">
+
+  <ItemVue v-for="servico, index in servicos" :key="index"
+  :item="servico"/>
+  </div>
+
+  </template>
+
+<script setup>
+  import ItemVue from "./components/Item.vue";
+</script>
+
+<script>
+import Dados from './db/services.json';
+export default {
+  data() {
+    return {
+      servicos: null,
+    }
+  },
+  mounted() {
+    this.servicos = Dados;
+    //console.log(Dados);
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+html {
+  background: url("https://cdn.pixabay.com/photo/2017/01/24/03/53/plant-2004483_960_720.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 100%;
+}
+
+.items {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
---
+++ vite-test/src/components/Item.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    <a :href="item.href">
+    <img :src="item.imagem" alt="Icone do Serviço"/>
+    </a>
+    <span>{{ item.titulo }}</span>
+    
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['item']
+}
+</script>
+
+<style scoped>
+    div {
+        margin: 20px 50px 20px 30px;
+        display: block;
+        width: 100px;
+    }
+
+    img {
+        width: 80px;
+    }
+    span{
+        font-weight: 900;
+    }
+
+    .card {
+        min-width: 120px;
+        min-height: 150px;
+        border: 1px solid grey;
+        border-radius: 5%;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        opacity: .9;
+        box-shadow: 0 0 2em grey;
+        transition: box-shadow .5s;
+    }
+
+    .card:hover {
+        opacity: 1;
+        box-shadow: 0 0 4em grey;
+    }
+
+</style>
