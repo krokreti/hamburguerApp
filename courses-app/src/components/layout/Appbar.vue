d4908f2251850e3f2854d41f5625fd4bb2087f02
davi <davipinheiro1993@gmail.com>
2022-11-10 13:02:32 -0300

    Finish Menu

--- courses-app/src/components/layout/Appbar.vue
+++ courses-app/src/components/layout/Appbar.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="appbar">
-    <span style="font-weight: bold;">Hey, Davi 👋 </span>
-      <button @click="openMenu">Click me</button>
+    <div style="display: flex;" >
+      <div class="appbar-button" @click="openMenu" >
+          <div class="bar1"></div>
+          <div class="bar2"></div>
+          <div class="bar3"></div>
+      </div> 
+      <span class="appbar-username"> Hey, Davi 👋 </span>  
+    </div>  
     <div class="appbar-img-profile" >
       <img src="../../assets/unknown-profile.jpg" class="appbar-img-profile-class" alt="Profile-Picture">
     </div>
@@ -45,6 +51,13 @@ export default defineComponent({
     align-items: center;
 }
 
+.appbar-username {
+    margin-left:1em;
+    font-weight: bold; 
+    display:flex; 
+    align-items:center;
+}
+
 .appbar-img-profile {
   display: flex;
   justify-content: center;
@@ -58,5 +71,18 @@ export default defineComponent({
   width: 2.5em;
   object-fit: cover;
   border: 3px solid transparent;
+}
+
+.appbar-button {
+    display: inline-block;
+    cursor: pointer;
+}
+
+.bar1, .bar2, .bar3 {
+  width: 35px;
+  height: 5px;
+  background-color: #333;
+  margin: 6px 0;
+  transition: 0.4s;
 }
 </style>
--- courses-app/src/components/layout/Menu.vue
+++ courses-app/src/components/layout/Menu.vue
@@ -1,14 +1,26 @@
 <template>
     <div class="menu-background" v-if="showMenu" @click="$emit('close')"></div>
     <transition name="master">
-        <div class="menu" v-if="showMenu">
+        <div class="menu" style="padding:1em;" v-if="showMenu">
             <div class="menu-header">
                 <div>
-                    AAAA
+                    <h3>Courses App</h3>
                 </div>
-                <div>
-                    <button class="btn " @click="$emit('close')"> X </button>
-                </div>
+            </div>
+            <hr>
+            <div class="menu-content" style="margin-top:1em;">
+                <router-link to="/">
+                    <div class="menu-content-button">
+                        <i class="fa-solid fa-house" style="margin-right:1em;"></i>
+                        <span> Home </span>
+                    </div>
+                </router-link>
+                <router-link to="/my-courses">
+                    <div class="menu-content-button">
+                        <i class="fa-sharp fa-solid fa-graduation-cap" style="margin-right:1em;"></i>
+                        <span class="font-bold"> My Courses </span>
+                    </div>
+                </router-link>
             </div>
         </div>
     </transition>
@@ -31,9 +43,12 @@ export default defineComponent({
     left: 0;
     height: 100vh;
     width: 75%;
-    background-image: linear-gradient(to right, rgba(184, 201, 207, 1), rgba(246, 250, 242, 1));
+    box-shadow: 10px 10px 5px rgba(0,0,0,0.4);
+    /* background-image: linear-gradient(to right, rgba(184, 201, 207, 1), rgba(246, 250, 242, 1)); */
+    /* background-color: #bc46f9; */
+    background-color: #214f77;
+    color: white;
     z-index: 2;
-    /* animation: enter 0.3s ease-out forwards; */
 }
 
 .master-enter-active {
@@ -56,27 +71,41 @@ export default defineComponent({
 }
 
 .menu-background {
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  padding-top: 100px; /* Location of the box */
+  position: fixed; 
+  z-index: 1; 
+  padding-top: 100px; 
   left: 0;
   top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  width: 100%; 
+  height: 100%;
+  overflow: auto; 
+  background-color: rgb(0,0,0); 
+  background-color: rgba(0,0,0,0.4); 
 }
 
 .menu-header {
     display: flex;
     justify-content: space-between;
-    padding: 2em;
+    align-items: center;
+    padding: 1.5em;
+}
+
+.menu-content-button {
+    display: flex;
+    justify-content: flex-start;
+    padding: 1em;
+    color: white;
 }
 
 @media (min-width: 700px) {
     .menu {
         width: 40%;
     }
+}
+</style>
+
+<style scoped>
+a {
+    text-decoration: none;
 }
 </style>
