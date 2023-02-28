e5050e29a163cbeae039879b50ebb249207b52c9
davi <davipinheiro1993@gmail.com>
2023-02-28 08:04:13 -0300

    New calendar

--- reusable_configs/src/main.js
+++ reusable_configs/src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 import store from './store'
 import 'v-calendar/dist/style.css';
 import VCalendar from 'v-calendar';
+import { ProCalendar } from '@lbgm/pro-calendar-vue';
 
 createApp(App).use(store).use(router).use(VCalendar, {
     componentPrefix: 'vc',
-}).mount('#app')
+}).use(ProCalendar).mount('#app')
---
+++ reusable_configs/src/views/NewCalendar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>New Calendar</h2>
+    <pro-calendar
+      :events="evts"
+      :loading="false"
+      :config="cfg"
+      view="week"
+      date="'isoStringDate'"
+    ></pro-calendar>
+  </div>
+  <!-- https://vuejsexamples.com/a-best-professional-calendar-ever-for-vue-js/ -->
+</template>
+
+<script>
+import "@lbgm/pro-calendar-vue/style";
+
+export default {
+  name: "NewCalendar",
+};
+</script>
+
+<style>
+</style>
